f8eac0f01eacae19847b6d0c941bafdbaeda698e
Ken Robertson <ken@invalidlogic.com>
2010-03-31 20:02:39 -0700

    Changed tests to use proper DM Serial

--- test/helper.rb
+++ test/helper.rb
@@ -49,7 +49,7 @@ def rebuild_model options = {}
     include DataMapper::Resource
     include DataMapper::Validate
     include Paperclip::Resource
-    property :id, Integer, :serial => true
+    property :id, Serial
     property :other, String
     has_attached_file :avatar, options
   end
